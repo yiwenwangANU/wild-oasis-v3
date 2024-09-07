c34b0b24998bd82e1b30b1cdf3a6e608d2145e87
admin01 <paradoxraphael@gmai.com>
2024-09-07 10:39:06 +1000

    update setting form

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,69 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import { useForm } from "react-hook-form";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
 
 function UpdateSettingsForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow
+        label="Minimum nights/booking"
+        error={errors?.min_nights?.message}
+      >
+        <Input
+          type="number"
+          id="min_nights"
+          {...register("min_nights", {
+            required: "This is required",
+            min: { value: 1, message: "Minimum nights should larger than 0" },
+          })}
+        />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow
+        label="Maximum nights/booking"
+        error={errors?.max_nights?.message}
+      >
+        <Input
+          type="number"
+          id="max_nights"
+          {...register("max_nights", {
+            required: "This is required",
+            min: { value: 1, message: "Maximum nights should larger than 0" },
+          })}
+        />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow
+        label="Maximum guests/booking"
+        error={errors?.max_guests?.message}
+      >
+        <Input
+          type="number"
+          id="max_guests"
+          {...register("max_guests", {
+            required: "This is required",
+            min: { value: 1, message: "Maximum guests should larger than 0" },
+          })}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price" error={errors?.breakfast_price?.message}>
+        <Input
+          type="number"
+          id="breakfast_price"
+          {...register("breakfast_price", {
+            required: "This is required",
+            min: { value: 1, message: "Breakfast price should larger than 0" },
+          })}
+        />
+      </FormRow>
+      <FormRow>
+        <Button type="submit">Update Settings</Button>
       </FormRow>
     </Form>
   );
